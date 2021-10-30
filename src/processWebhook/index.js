27a8f6aa50df82341965a984fb1730c4adcba8e8
Rob Anderson <riznob@gmail.com>
2021-10-30 13:06:32 -0600

    validate secret

--- src/processWebhook/index.js
+++ src/processWebhook/index.js
@@ -1,9 +1,18 @@
 const { Octokit } = require("@octokit/rest");
+const crypto = require("crypto");
+const AWS = require('aws-sdk');
+const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
 
 exports.handler = async (event, context, callback) => {
-    console.log('Received event:', JSON.stringify(event, null, 2));
-    let body = JSON.parse(decodeURIComponent(event.body.substring(event.body.indexOf('=')+1)));
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+    
+    if (validateJsonWebhook(event)) {
+        console.log("Webhook signature valid");
+        let body = JSON.parse(decodeURIComponent(event.body.substring(event.body.indexOf('=')+1)));
 
+    } else {
+        console.log("Webhook signature invalid");
+    }
     const responseBody = {
         status: "success"
     };
@@ -19,3 +28,24 @@ exports.handler = async (event, context, callback) => {
     };
     callback(null, response);
 };
+
+function validateJsonWebhook(event) {
+
+    // calculate the signature
+    const expectedSignature = "sha1=" +
+        crypto.createHmac("sha1", process.env.SECRET)
+            .update(event.body)
+            .digest("hex");
+
+    // compare the signature against the one in the request
+    const signature = event.headers["X-Hub-Signature"];
+    if (signature !== expectedSignature) {
+        //throw new Error("Invalid signature.");
+        return false;
+    } else{
+        return true;
+    }
+
+}
+
+exports.validateJsonWebhook = validateJsonWebhook;

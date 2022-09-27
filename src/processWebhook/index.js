178c59624830f0c722c313db0aac30f3a6ed5add
Rob Anderson <riznob@gmail.com>
2022-09-27 06:04:19 +0000

    more options

--- src/processWebhook/index.js
+++ src/processWebhook/index.js
@@ -16,20 +16,39 @@ exports.handler = async (event, context, callback) => {
                 repo: body.repository.name,
                 branch: body.repository.default_branch,
                 required_status_checks: null,
-                enforce_admins: true,
                 required_pull_request_reviews: {
-                    required_approving_review_count: 1
+                    dismiss_stale_reviews: process.env.DISMISS_STALE_REVIEWS  == 'true' ? true : false,
+                    require_code_owner_reviews: process.env.REQUIRE_CODE_OWNER_REVIEWS == 'true' ? true : false,
+                    required_approving_review_count: process.env.REQUIRED_APPROVING_REVIEW_COUNT ? parseInt(process.env.REQUIRED_APPROVING_REVIEW_COUNT) : null,
                 },
                 restrictions: {
                     users: [],
                     teams: []
-                }
+                },
+                enforce_admins: process.env.ENFORCE_ADMINS == 'true' ? true : false,
+                required_linear_history: process.env.REQUIRED_LINEAR_HISTORY == 'true' ? true : false,
+                allow_force_pushes: process.env.ALLOW_FORCE_PUSHES == 'true' ? true : false,
+                allow_deletions: process.env.ALLOW_DELETIONS == 'true' ? true : false,
+                block_creations: process.env.BLOCK_CREATIONS == 'true' ? true : false,
+                required_conversation_resolution: process.env.REQUIRED_CONVERSATION_RESOLUTION == 'true' ? true : false,
             });
             octokit.rest.issues.create({
                 owner: body.repository.owner.login,
                 repo: body.repository.name,
                 title: "Branch protections added",
-                body: `Branch protections added to the default branch include:  \n - Require a pull request before merging  \n - Required number of approvals before merging: 1  \n - Restrict who can push to matching branches  \n - Enforce restrictions for administrators  \n@${process.env.NOTIFY_USER}`,
+                body: `Branch protections setting:  \n
+ - Require a pull request before merging  \n
+ - Dismiss stale pull request approvals when new commits are pushed: ${process.env.DISMISS_STALE_REVIEWS ? process.env.DISMISS_STALE_REVIEWS : false}  \n
+ - Require review from Code Owners: ${process.env.REQUIRE_CODE_OWNER_REVIEWS ? process.env.REQUIRE_CODE_OWNER_REVIEWS : false}  \n
+ - Required number of approvals before merging: ${process.env.REQUIRED_APPROVING_REVIEW_COUNT ? process.env.REQUIRED_APPROVING_REVIEW_COUNT : 0}  \n
+ - Enforce Admins: ${process.env.ENFORCE_ADMINS ? process.env.ENFORCE_ADMINS : false}  \n
+ - Require linear history: ${process.env.REQUIRED_LINEAR_HISTORY ? process.env.REQUIRED_LINEAR_HISTORY : false}  \n
+ - Allow force pushes: ${process.env.ALLOW_FORCE_PUSHES ? process.env.ALLOW_FORCE_PUSHES : false}  \n
+ - Allow deletions: ${process.env.ALLOW_DELETIONS ? process.env.ALLOW_DELETIONS : false}  \n
+ - Restrict new branch creation: ${process.env.BLOCK_CREATIONS ? process.env.BLOCK_CREATIONS : false}  \n 
+ - Enforce restrictions for administrators: ${process.env.ENFORCE_ADMINS ? process.env.ENFORCE_ADMINS : false}  \n
+ - Require conversation resolution: ${process.env.REQUIRED_CONVERSATION_RESOLUTION ? process.env.REQUIRED_CONVERSATION_RESOLUTION : false}  \n
+ @${process.env.NOTIFY_USER}`,
             });
         } else {
             console.log("Not the event we are looking for...");
